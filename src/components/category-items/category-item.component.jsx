e42b9d9a34de3329321ace96160ceb35d68346c2
prashantkumarkaushik <prashantkaushik42@gmail.com>
2022-10-04 15:02:11 +0530

    Adding category item

--- src/components/category-items/category-item.component.jsx
+++ src/components/category-items/category-item.component.jsx
@@ -10,7 +10,7 @@ const CategoryItem = ({category}) => {
             }}  
           />
           <div className="category-body-container">
-            <h2>{title}</h2>
+            <h2>{title.toUpperCase()}</h2>
             <p>Shop Now</p>
           </div>
         </div>
@@ -18,3 +18,4 @@ const CategoryItem = ({category}) => {
 }
 
 export default CategoryItem
+
